5d274bad1cf52f3eb86559223786342d8a782f23
Văn Dân <danok123456@gmail.com>
2022-12-10 01:01:55 +0700

    add report 2

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,22 @@
 
 //Create Table
+export const CreateTableUrl = "https://zluc5n334a6m7t4li2qpfckwqi0eafju.lambda-url.us-east-1.on.aws/"
+
+export const GetAllTableByUserIDUrl = "https://xz5wrdnpasupvudszinshirrwi0kviim.lambda-url.us-east-1.on.aws/"
+
+export const GetAllItemByTableNameUrl = "https://5uhumqbwf5aonvxfg6qewtpvii0shoih.lambda-url.us-east-1.on.aws/"
+
+export const LoginUrl = "https://r4xhtn24kdbc7yfnsydl3wirxu0hfuyd.lambda-url.us-east-1.on.aws/"
+
+export const ReigsterUrl = "https://fd6calajak76kdqqxu6xoiy2oy0sjagv.lambda-url.us-east-1.on.aws/"
+
+export const AddAndUpdateItemUrl = "https://glod62qmveejwjmed2y4myo3bi0vardd.lambda-url.us-east-1.on.aws/"
+
+export const DeleteTableUrl = "https://ykk77wsja262k4epn2nwhjwm6e0bzyzu.lambda-url.us-east-1.on.aws/"
+
+export const DeleteItemUrl = "https://pp3bbdbfuf4cciv5cpwnokxf2e0jxprh.lambda-url.us-east-1.on.aws/"
+
+/*
 export const CreateTableUrl = "https://gwyhy6c3xlr4vhhwxxasdkao4y0scqzo.lambda-url.us-east-1.on.aws/"
 
 export const GetAllTableByUserIDUrl = "https://dprfr47bzmyqspn6jmkz56auka0gbeot.lambda-url.us-east-1.on.aws/"
@@ -15,4 +32,4 @@ export const AddAndUpdateItemUrl = "https://ho4f6dojfpf4hlf3cbwl2yj3rq0oxowk.lam
 export const DeleteTableUrl = "https://tkuuvwpcj3exsznid6ydglc7ce0egmbs.lambda-url.us-east-1.on.aws/"
 
 export const DeleteItemUrl = "https://tfyfwokjtrinxe6yrz5y56syfm0petbl.lambda-url.us-east-1.on.aws/"
-
+*/
